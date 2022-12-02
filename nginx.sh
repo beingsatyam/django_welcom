ff3a63384b115369877ce041a1bea830d2ddd45e
beingsatyam <satyam.dubey717@gmail.com>
2022-12-02 15:35:21 +0530

    nginx sites en mod

--- nginx.sh
+++ nginx.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/conf.d
+sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 710 /var/lib/jenkins/workspace/django_welcome_ci_cd/
+
+sudo ln -s /etc/nginx/sites-available/app  /etc/nginx/sites-enabled
 
 sudo nginx -t 
 
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-echon "nginx has been started"
+echo "nginx has been started"
 
-sudo systemctl start nginx
+sudo systemctl status nginx
